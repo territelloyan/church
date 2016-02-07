4a701a0424f15df4d4531f2dcb0607b5526b46c3
bennettscience <brian.bennett2@gmail.com>
2016-02-06 22:10:31 -0500

    -vid class, script updated to MVP
    `vid.js` now recognizes which cell is clicked to play the correct video.
    This allows you to use a class as the click handler and only require videos to have their own ID.

--- vid.js
+++ vid.js
@@ -1,14 +1,19 @@
 $(document).ready(function() {
-  $('#vid').on('click', function() {
-    var v = document.getElementById("vid1");
+  $('.cell').on('click', function() {
+    var element = this.getElementsByTagName('video');
+    var m = element[0].getAttribute('id');
+
+    console.log(m);
+    var v = document.getElementById(m);
     if (v.webkitRequestFullscreen) {
       v.className = "";
       v.webkitRequestFullscreen();
     }
     v.play();
-  })
-  $("#vid1").on("ended", function() {
-    this.webkitExitFullscreen();
-    this.className = "hide";
+
+    $("#" + m).on("ended", function() {
+      this.webkitExitFullscreen();
+      this.className = "hide";
+    });
   })
 })
