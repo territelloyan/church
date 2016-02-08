a6363b56b566577177d1210e450b39ff179be875
bennettscience <brian.bennett2@gmail.com>
2016-02-07 21:57:10 -0500

    hide video when user closes.
    Still needs to properly pause the video when exited by the user.

--- vid.js
+++ vid.js
@@ -8,12 +8,19 @@ $(document).ready(function() {
     if (v.webkitRequestFullscreen) {
       v.className = "";
       v.webkitRequestFullscreen();
+      v.play();
     }
-    v.play();
+    // v.play();
 
     $("#" + m).on("ended", function() {
       this.webkitExitFullscreen();
       this.className = "hide";
     });
+
+    // Listen for exiting fullscreen, hide video element.
+    $("#" + m).on("webkitfullscreenchange", function(e) {
+      this.className = "hide";
+      // v.pause();
+    })
   })
 })
