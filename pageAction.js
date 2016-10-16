2c55c8c12ddbf6acba59a1314c4abc475bab73b1
bennettscience <brian.bennett2@gmail.com>
2016-10-15 21:06:09 -0400

    update styles, rework overlay, +content

--- pageAction.js
+++ pageAction.js
@@ -1,11 +1,4 @@
 $(document).ready(function() {
-  // Animate the content on page load.
-  // setTimeout(function() {
-  //   $("#logo").animate({"height":"120px"},1000);
-  //   setTimeout(function(){
-  //     $(".cell,#credits").animate({"opacity":"100"},2000);
-  //   },2000)},2000);
-
   var i = null;
   $("#body-wrap").mousemove(function() {
     clearTimeout(i);
