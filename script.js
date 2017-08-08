141498296b080e91520a36aff08f64bd4197044c
Brian E Bennett <brian@ohheybrian.com>
2017-08-07 21:28:51 -0400

    Started constructor, needs refactor and testing

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 var Card = function(name, bio, img, vid) {
-  var container = document.getElementById('#page__block--right');
-  var card = document.createElement('div');
-  this.name = "<h2 class-'card__header'>" + name + "</h2>";
-  this.bio = "<div class='card__bio'>" + bio + "</div>"
-  this.img = "<div class='card__img'><img src='" + img + "' /></div>";
-  this.vid = "<div class='card__vid'>" + vid + "</div>";
+  this.name = name;
+  this.bio = bio;
+  this.img = img;
+  this.vid = vid;
 
-  card.className = "card";
+  this.create = function() {
+    var container = document.getElementById('page__block--right');
+    var card = document.createElement('div');
+    card.className = "card";
 
-  // this.create = function() {
-  //   container.appendChild(card);
-  // }
+    var title = document.createElement('h2');
+    title.className = "card__header";
+    title.innerHTML = this.name
+
+    var bio = document.createElement('div');
+    bio.className = "card__bio";
+    bio.innerHTML = this.bio;
+
+    var img = document.createElement('img');
+    img.className = "card__img";
+    img.src = this.img;
+
+    var vid = document.createElement('video');
+    vid.className = "card__vid";
+
+    card.name = title;
+    card.bio = bio;
+    card.img = img;
+    card.vid = vid;
+    
+    container.appendChild(card);
+    console.log(card);
+  }
 }
